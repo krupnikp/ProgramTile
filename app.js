76144e32ed780a60cdb296bafe281dc1dce2bd5e
Piotr Krupnik <pkrupnik@PiotrKrupnik.local>
2019-07-04 16:24:00 +0200

    timer change logic

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
-const countDownTime = new Date('2019-07-04 12:08:30').getTime();
-console.log(countDownTime)
+const startDate = +new Date('2019-07-04 16:25:30');
 
 
-const timer = () => {
-    const now = new Date().getTime();
+const timer = (start) => {
+    const now = Date.now();
 
-    const distance = countDownTime - now;
+    const distance = Math.floor((startDate - now) / (60 * 1000));
+    
+    const minutes = distance % (1000 * 60);
 
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // console.log(`${minutes}:${seconds}`)
-    // console.log(distance)
-
-    switch (true) {
-        case (distance <= 600000 && distance >= 60000):
-        document.getElementsByClassName("col-header-timer")[0].innerHTML = `Start za: ${minutes}min`;
-        break;
-        case (distance < 60000):
+    if (distance <= 10 && distance >= 1) {
+        Array.from(document.getElementsByClassName("col-header-timer"), 
+        e => e.innerHTML = `Start za ${minutes} min`)
+    } else if (distance < 1) {
         clearInterval(timer);
-        document.getElementsByClassName("col-header-timer")[0].innerHTML = "Trwa";
-        break;
+        Array.from(document.getElementsByClassName("col-header-timer"),
+        e => e.innerHTML = "Trwa");
     }
 };
 window.onload = timer
 setInterval(timer, 60000)
+
