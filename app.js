b86abcc5c794fca71a54b7489b26c89a72bd5837
Piotr Krupnik <pkrupnik@PiotrKrupnik.local>
2019-07-04 11:42:17 +0200

    countdown v2

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const countDownTime = new Date('2019-07-05 16:45:30').getTime();
+const countDownTime = new Date('2019-07-04 11:40:30').getTime();
 console.log(countDownTime)
 
 
@@ -12,17 +12,21 @@ const timer = setInterval(function(){
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     console.log(`${minutes}:${seconds}`)
-    document.getElementsByClassName("col-header-timer")[0].innerHTML = `${hours}:${minutes}:${seconds}`
-
-  if (distance < 0) {
-    clearInterval(timer);
-    document.getElementsByClassName("col-header-timer")[0].innerHTML = "Trwa";
-  }
-
+    // document.getElementsByClassName("col-header-timer")[0].innerHTML = `${hours}:${minutes}:${seconds}`
+    console.log(distance)
+    switch (true) {
+        case (distance <= 600000 && distance >= 1):
+        document.getElementsByClassName("col-header-timer")[0].innerHTML = `Start za: ${minutes}min`;
+        break;
+        case (distance < 0):
+        clearInterval(timer);
+        document.getElementsByClassName("col-header-timer")[0].innerHTML = "Trwa";
+        break;
+    }
 },1000);
 
-// if (distance < 1800) { //od 30min
-//     document.getElementsByClassName("col-header-timer")[0].innerHTML = `${hours}:${minutes}:${seconds}`
+// if (distance <= 1800) { //od 30min
+//     document.getElementsByClassName("col-header-timer")[0].innerHTML = `Start za: ${minutes}:${seconds}min`
 // } else if (distance < 0) {
 //     clearInterval(timer);
 //     document.getElementsByClassName("col-header-timer")[0].innerHTML = "Trwa";
