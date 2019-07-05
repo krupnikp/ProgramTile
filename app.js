9f0e9a936a95ae3c6a4496d60d45c067eda494f5
Piotr Krupnik <pkrupnik@PiotrKrupnik.local>
2019-07-05 15:55:36 +0200

    countDown elements

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-const startDate = +new Date('2019-07-05 14:55:30')
+const startDates = [
+    '2019-07-05 15:21:30',
+    '2019-07-05 15:30:30'
+]
 
 const timer = (htmlEl, startTime) => {
-    setInterval(() => {
+   const intervalId = setInterval(() => {
         const timeNow = Date.now();
-
-        const distanceInMinutes = Math.floor((startTime - timeNow) / (60 * 1000));
+        console.log('hello')
+        const distanceInMinutes = Math.floor((+new Date(startTime) - timeNow) / (60 * 1000));
         
         if (distanceInMinutes <= 10 && distanceInMinutes >= 1) {
             htmlEl.innerHTML = `Start za ${distanceInMinutes} min`;
         } else if (distanceInMinutes < 1) {
             htmlEl.innerHTML = "Trwa";
+            clearInterval(intervalId)
         }
     }, 1000)
-    clearInterval(timer)
 };
 // window.onload = timer
 // setInterval(timer(startDate), 60000)
 
-[...document.getElementsByClassName("col-header-timer")].forEach(el => {
-    timer(el, startDate)
+[...document.getElementsByClassName("col-header-timer")].forEach((el, index) => {
+    timer(el, startDates[index])
 })
 
