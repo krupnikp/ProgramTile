a4eec852354e8fc7bae098f8970f5920bd5d08a1
Piotr Krupnik <pkrupnik@PiotrKrupnik.local>
2019-07-05 14:52:30 +0200

    fn timer

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
-const startDate = +new Date('2019-07-05 11:30:30');
-const startDate2 = +new Date('2019-07-04 18:55:30');
-
+const startDate = +new Date('2019-07-05 14:55:30')
 
 const timer = (htmlEl, startTime) => {
-    const timeNow = Date.now();
+    setInterval(() => {
+        const timeNow = Date.now();
 
-    const distanceInMinutes = Math.floor((startTime - timeNow) / (60 * 1000));
-    
-    if (distanceInMinutes <= 10 && distanceInMinutes >= 1) {
-        htmlEl.innerHTML = `Start za ${distanceInMinutes} min`;
-    } else if (distanceInMinutes < 1) {
-        htmlEl.innerHTML = "Trwa";
-    }
+        const distanceInMinutes = Math.floor((startTime - timeNow) / (60 * 1000));
+        
+        if (distanceInMinutes <= 10 && distanceInMinutes >= 1) {
+            htmlEl.innerHTML = `Start za ${distanceInMinutes} min`;
+        } else if (distanceInMinutes < 1) {
+            htmlEl.innerHTML = "Trwa";
+        }
+    }, 1000)
+    clearInterval(timer)
 };
-window.onload = timer
-setInterval(timer(startDate), 1000)
-// setInterval(timer(startDate2), 1000)
+// window.onload = timer
+// setInterval(timer(startDate), 60000)
 
-document.getElementsByClassName("col-header-timer")[0]
-document.getElementsByClassName("col-header-timer")[0]
+[...document.getElementsByClassName("col-header-timer")].forEach(el => {
+    timer(el, startDate)
+})
 
