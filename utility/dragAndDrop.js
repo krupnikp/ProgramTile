f9f5ce6c51cb5bf2e8eea5c0140e30ed2cb8568a
Piotr Krupnik <pkrupnik@PiotrKrupnik.local>
2019-07-24 15:13:35 +0200

    animations on drop and missing

--- utility/dragAndDrop.js
+++ utility/dragAndDrop.js
@@ -1,8 +1,10 @@
 const sizeTopTargetShow = '300px';
 const sizeBottomTargetShow = '300px';
+const sizeTargetInitial = '80px';
 
 const sizeTopViewShow = '211px';
 const sizeBottomViewShow = '211px';
+const sizeViewInitial = '0px';
 
 const dragBottomTargets = document.querySelector('.over-bottom-target');
 const dragTopTargets = document.querySelector('.over-top-target');
@@ -11,12 +13,14 @@ let targetTop = null;
 let targetBottom = null;
 
 let draggableElement = null;
+let dropFailed = false;
 let catchedTargetLi = null;
 
 let nextElement = null;
-let previousElemen = null;
+let previousElement = null;
 
 function handleDragStart(event) {
+    dropFailed = true;
     draggableElement = event.target;
 
     nextElement = draggableElement.nextElementSibling;
@@ -25,16 +29,11 @@ function handleDragStart(event) {
     draggableElement.querySelector('.over-top-target').style.display = 'none';
     draggableElement.querySelector('.over-bottom-target').style.display = 'none';
 
-    if (previousElement) {
-        previousElement.querySelector('.over-bottom-target').style.display = 'none';
-    }
-    if (nextElement) {
-        nextElement.querySelector('.over-top-target').style.display = 'none';
-    }
+    closestTargetHide(previousElement, nextElement)
     this.style.opacity = '0.3';
 }
 
-function handleDragOver(event){
+function handleDragOver(event) {
     event.preventDefault();
 }
 
@@ -56,40 +55,55 @@ function handleDragEnter(event) {
 function handleDragLeave(event) {
     event.preventDefault();
 
-    this.parentElement.style.height = '0px';
-    this.style.height = '80px';
-
-    this.parentElement.style.height = '0px';
-    this.style.height = '80px';
+    this.parentElement.style.height = sizeViewInitial;
+    this.style.height = sizeTargetInitial;
 }
 
 function handleDrop(event) {
     event.preventDefault();
     catchedTargetLi = event.target.closest('li');
+    dropFailed = false;
+    draggableElement.classList.add('animationDrop');
+    removeClass('animationDrop', 800)
 
     if (event.target.closest('.over-top')) {
-        catchedTargetLi.parentElement.insertBefore(draggableElement, catchedTargetLi)
-        targetTop.style.height = '0px';
-        targetTop.children[0].style.height = '80px';
+        catchedTargetLi.parentElement.insertBefore(draggableElement, catchedTargetLi);
+        targetTop.style.height = sizeViewInitial;
+        targetTop.children[0].style.height = sizeTargetInitial;
     } else if (event.target.closest('.over-bottom')) {
         catchedTargetLi.parentElement.insertBefore(draggableElement, catchedTargetLi.nextElementSibling)
-        targetBottom.style.height = '0px';
-        targetBottom.children[0].style.height = '80px';
+        targetBottom.style.height = sizeViewInitial;
+        targetBottom.children[0].style.height = sizeTargetInitial;
     }
 }
 
 function handleDragEnd() {
+    if(dropFailed){
+        draggableElement.classList.add('animationDropFail');
+        removeClass('animationDropFail', 600)
+    }
     this.style.opacity = '1';
     draggableElement.querySelector('.over-top-target').style.display = 'initial';
     draggableElement.querySelector('.over-bottom-target').style.display = 'initial';
 
+    closestTargetHide(previousElement, nextElement)
+}
+
+function removeClass(className, timeout){
+    setTimeout(() => draggableElement.classList.remove(className), timeout);
+}
+
+function closestTargetHide(previousElement, nextElement) {
     if (previousElement) {
-        previousElement.querySelector('.over-bottom-target').style.display = 'initial';
+        const prevElBottomTarget = previousElement.querySelector('.over-bottom-target')
+        prevElBottomTarget.style.display = prevElBottomTarget.style.display === 'none' ? 'initial' : 'none';
     }
     if (nextElement) {
-        nextElement.querySelector('.over-top-target').style.display = 'initial';
+        const prevElTopTarget = nextElement.querySelector('.over-top-target')
+        prevElTopTarget.style.display = prevElTopTarget.style.display === 'none' ? 'initial' : 'none';
     }
 }
+
 
 function dragAndDrop(el) {
     el.addEventListener('dragstart', handleDragStart);
